6864119a16c061a7ca9dea3c0ac6287bb378ba48
Eleven <2944670348@qq.com>
2026-07-27 17:31:26 +0800

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://Eleven11-byte.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
